a17ee6883413bc075a53957091507f48a8422943
Dhirajk56 <Dhirajkumarxyz13@gmail.com>
2025-05-13 22:25:14 +0530

    cors added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connect");
 const userRoutes = require("./routes/user");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRoutes);
 
